cfaf79224a0af3cc0479a9596b8a1c8ddd768b62
Juciara-Silva <juciara2357@gmail.com>
2025-04-29 18:00:15 -0300

    atualizando

---
+++ 2025-04-29/teste1.py
@@ -0,0 +1,12 @@
+print('calcular área do circulo')
+
+raio = 5.13
+
+area = 3.14 * raio ** 2
+
+print('Área do circulo: ', area )
+
+print('A area do circulo de raio', raio, 'é', area )
+
+print(f'A area do circulo de raio {raio} é {area}')
+
