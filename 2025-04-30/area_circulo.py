bbf71244a179592bd9bc4f1bb30b1ac1de5db643
Juciara-Silva <juciara2357@gmail.com>
2025-04-30 17:40:38 -0300

    salvar

---
+++ 2025-04-30/area_circulo.py
@@ -0,0 +1,11 @@
+#progama para calcular a area do circuo
+raio = float(input('Digite o raio do circulo:'))
+
+area = 3.14 * raio ** 2
+
+print(f'A area do circulo é {area}')
+
+
+comprimento = 2 * 3.14 *  raio
+
+print(f'O comprimento da circuferencia é {comprimento}')
---
+++ 2025-04-30/escreve-texto.py
@@ -0,0 +1,5 @@
+# programa para imprimir um texto na tela
+
+nome = input('Digite o seu nome: ')
+
+print(f'Ola {nome}!!!')
