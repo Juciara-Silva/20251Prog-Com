6a078d880c0832ae7a4563e1d38e062c0efd4494
Juciara-Silva <juciara2357@gmail.com>
2025-05-28 08:55:21 -0300

    questab&produto

---
+++ 2025-05-27/prodto.py
@@ -0,0 +1,25 @@
+import sys
+
+try:
+    x = int(input('Digite o primeiro número inteiro positivo: '))
+    y = int(input('Digite o segundo número inteiro positivo: '))
+
+    if x <= 0 or y <= 0:
+        sys.exit('Informe apenas valores inteiros positivos')
+
+    resultado = 0
+    contador = 0
+    expressao = ""
+
+    while contador < y:
+        resultado += x
+        expressao += f"{x} + " if contador < y - 1 else f"{x}" 
+        contador += 1
+
+    print(f'{expressao}, totalizando {resultado}.')
+
+except ValueError:
+    sys.exit('Informe apenas valores inteiros positivos')
+'''
+y é quantas vezes x vai ser repetido
+'''
