6d71e8630abbef4ea2b02f067ee2cb9d75726529
Juciara-Silva <juciara2357@gmail.com>
2025-07-18 09:07:02 -0300

    ques

--- 2025-07-10/exer.py
+++ 2025-07-10/exer.py
@@ -7,10 +7,12 @@ strDir = os.path.dirname(__file__)
 cotacoes = []
 # Abrindo e lendo o arquivo
 try:
+    
     with open(f'{strDir}\\cotacao_dolar.csv', 'r', encoding='utf-8') as arqLeitura:
         leitor = csv.reader(arqLeitura, delimiter=';')
         # Ignorando o cabeçalho
         next(leitor)
+        
         for linha in leitor:
             # Convertendo os dois primeiros valores para float e o terceiro para data
             cotacao_compra = float(linha[0].replace(',', '.'))
@@ -18,6 +20,7 @@ try:
             data_hora_cotacao = datetime.strptime(linha[2], '%d/%m/%Y')
             # Adicionando os dados à lista
             cotacoes.append([cotacao_compra, cotacao_venda, data_hora_cotacao])
+
 except FileNotFoundError:
     print('Erro: arquivo não encontrado')
 except Exception as erro:
